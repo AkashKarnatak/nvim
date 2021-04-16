76d3d442af21d6025c67ec693f1e3af22b58f021
Akash Karnatak <akashkarnatak.ak47@gmail.com>
2021-04-16 13:12:21 +0530

    Fixed autocompletion path bug

--- lua/completion-nvim.lua
+++ lua/completion-nvim.lua
@@ -7,16 +7,21 @@ vim.g.completion_enable_snippet = 'Neosnippet'
 vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
 vim.g.completion_matching_smart_case = 1
 vim.g.completion_trigger_on_delete = 1
+vim.g.completion_sorting = "alphabet"
 vim.g.completion_auto_change_source = 1
-vim.g.completion_trigger_keyword_length=3
+-- vim.g.completion_trigger_keyword_length=10000
+-- vim.g.completion_enable_auto_popup = 0
+-- vim.g.completion_enable_auto_hover = 0
+-- vim.g.completion_enable_auto_signature = 0
 
 vim.g.completion_chain_complete_list = {
   default = {
-    { complete_items = {'lsp', 'snippet', 'path'} },
+    { complete_items = { 'lsp', 'snippet' } },
+    { complete_items = { 'path' } },
     { mode = '<c-p>' },
     { mode = '<c-n>' },
   }
 }
 
-vim.cmd([[imap <silent> <C-Space> <Plug>(completion_trigger)]])
+vim.api.nvim_set_keymap('i', '<C-Space>', '<Plug>(completion_trigger)', {silent = true})
 vim.cmd([[autocmd BufEnter * lua require'completion'.on_attach()]])
