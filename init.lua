b2af93ff35059c0fe92440453e4d30f1fe7c8c3d
Akash Karnatak <akashkarnatak.ak47@gmail.com>
2021-08-21 12:16:07 +0530

    Created colorizer settings file

--- init.lua
+++ init.lua
@@ -24,24 +24,10 @@ require('gitsign')
 require('telescope_')
 require('rooter_')
 require('statusline')
+require('nvim_colorizer_')
 
 ------------------- LSP ---------------------
 require('lsp/python-ls')
 require('lsp/cxx-ls')
 require('lsp/lua-ls')
 require('lsp/racket-ls')
-
-require 'colorizer'.setup {
-  less = {
-	RGB      = true;         -- #RGB hex codes
-	RRGGBB   = true;         -- #RRGGBB hex codes
-	names    = true;         -- "Name" codes like Blue
-	RRGGBBAA = true;        -- #RRGGBBAA hex codes
-	rgb_fn   = true;        -- CSS rgb() and rgba() functions
-	hsl_fn   = true;        -- CSS hsl() and hsla() functions
-	css      = true;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-	css_fn   = true;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
-	-- Available modes: foreground, background
-	mode     = 'background'; -- Set the display mode.
-  }
-}
