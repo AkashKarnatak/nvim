21733f5840dc171f900080100a9ebed2c9c2745f
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2022-09-11 19:59:06 +0530

    Silent mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -19,19 +19,19 @@ vim.api.nvim_set_keymap('n', '<C-Up>', '<C-w>+', {noremap = true, silent = true}
 vim.api.nvim_set_keymap('n', '<C-Right>', '<C-w>>', {noremap = true, silent = true})
 
 -- Remap 0, $, ^
-vim.api.nvim_set_keymap('n', '0', '$',  {noremap = true})
-vim.api.nvim_set_keymap('n', '$', '^',  {noremap = true})
-vim.api.nvim_set_keymap('n', '^', '0',  {noremap = true})
-vim.api.nvim_set_keymap('v', '0', '$h', {noremap = true})
-vim.api.nvim_set_keymap('v', '$', '^',  {noremap = true})
-vim.api.nvim_set_keymap('v', '^', '0',  {noremap = true})
-vim.api.nvim_set_keymap('o', '0', '$',  {noremap = true})
-vim.api.nvim_set_keymap('o', '$', '^',  {noremap = true})
-vim.api.nvim_set_keymap('o', '^', '0',  {noremap = true})
+vim.api.nvim_set_keymap('n', '0', '$',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '$', '^',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '^', '0',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '0', '$h', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '$', '^',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '^', '0',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', '0', '$',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', '$', '^',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', '^', '0',  {noremap = true, silent = true})
 
 -- Swap ^ and !
-vim.api.nvim_set_keymap('n', '!', '0',  {noremap = true})
-vim.api.nvim_set_keymap('n', '^', '!',  {noremap = true})
+vim.api.nvim_set_keymap('n', '!', '0',  {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '^', '!',  {noremap = true, silent = true})
 
 -- Remap g0, g$, g^
 vim.api.nvim_set_keymap('n', 'g0', 'g$',  {noremap = true, silent = true})
@@ -46,23 +46,23 @@ vim.api.nvim_set_keymap('o', 'g^', 'g0',  {noremap = true, silent = true})
 
 -- Custom text surround integration
 for _, char in ipairs({'_', '.', ':', ',', ';', '|', '/', '\\', '*', '+', '%'}) do
-    vim.api.nvim_set_keymap('x', 'i'..char, ':<C-u>normal! T'..char..'vt'..char..'<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('o', 'i'..char, ':normal vi'..char..'<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('x', 'a'..char, ':<C-u>normal! F'..char..'vf'..char..'<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('o', 'a'..char, ':normal va'..char..'<CR>', {noremap = true})
+    vim.api.nvim_set_keymap('x', 'i'..char, ':<C-u>normal! T'..char..'vt'..char..'<CR>', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('o', 'i'..char, ':normal vi'..char..'<CR>', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('x', 'a'..char, ':<C-u>normal! F'..char..'vf'..char..'<CR>', {noremap = true, silent = true})
+    vim.api.nvim_set_keymap('o', 'a'..char, ':normal va'..char..'<CR>', {noremap = true, silent = true})
 end
 
 -- << << Text object for CPP
-vim.api.nvim_set_keymap('x', 'io', [[":<C-u>execute \"normal ?<<\\\<CR\>3lv/<<\\\<CR\>2h\"<CR>"]], {expr=true, noremap = true})
-vim.api.nvim_set_keymap('o', 'io', [[":execute \"normal ?<<\\\<CR\>3lv/<<\\\<CR\>2h\"<CR>"]], {expr=true, noremap = true})
-vim.api.nvim_set_keymap('x', 'ao', [[":<C-u>execute \"normal ?<<\\\<CR\>v/<<\\\<CR\>l\"<CR>"]], {expr=true, noremap = true})
-vim.api.nvim_set_keymap('o', 'ao', [[":execute \"normal ?<<\\\<CR\>v/<<\\\<CR\>l\"<CR>"]], {expr=true, noremap = true})
+vim.api.nvim_set_keymap('x', 'io', [[":<C-u>execute \"normal ?<<\\\<CR\>3lv/<<\\\<CR\>2h\"<CR>"]], {expr=true, noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', 'io', [[":execute \"normal ?<<\\\<CR\>3lv/<<\\\<CR\>2h\"<CR>"]], {expr=true, noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'ao', [[":<C-u>execute \"normal ?<<\\\<CR\>v/<<\\\<CR\>l\"<CR>"]], {expr=true, noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', 'ao', [[":execute \"normal ?<<\\\<CR\>v/<<\\\<CR\>l\"<CR>"]], {expr=true, noremap = true, silent = true})
 
 -- Entire file text object
-vim.api.nvim_set_keymap('x', 'ie', ':<C-u>normal! ggvG<CR>', {noremap = true})
-vim.api.nvim_set_keymap('o', 'ie', ':normal ggvG<CR>', {noremap = true})
-vim.api.nvim_set_keymap('x', 'ae', ':<C-u>normal! ggVG<CR>', {noremap = true})
-vim.api.nvim_set_keymap('o', 'ae', ':normal ggVG<CR>', {noremap = true})
+vim.api.nvim_set_keymap('x', 'ie', ':<C-u>normal! ggvG<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', 'ie', ':normal ggvG<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'ae', ':<C-u>normal! ggVG<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', 'ae', ':normal ggVG<CR>', {noremap = true, silent = true})
 
 -- Tab switch buffer
 vim.api.nvim_set_keymap('n', 'L', ':bnext<CR>', {noremap = true, silent = true})
