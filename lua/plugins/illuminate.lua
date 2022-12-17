30397e1d085dbbd60e989d65259b3cf03a38d30b
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2022-12-17 13:38:59 +0530

    Added filetypes to illuminate blacklist

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -2,5 +2,17 @@ require('illuminate').configure({
   filetypes_denylist = {
     '',
     'TelescopePrompt',
+    "NvimTree",
+    "packer",
+    "netrw",
+    "dapui_scopes",
+    "dapui_breakpoints",
+    "dapui_stacks",
+    "dapui_watches",
+    "dap-repl",
+    "toggleterm",
+    "input",
+    "output",
+    "lspinfo",
   },
 })
