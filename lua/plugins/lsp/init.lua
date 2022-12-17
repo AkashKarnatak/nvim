7a4610c70fa7322edce6e27c188bfa244cc61ee3
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2022-12-17 22:05:46 +0530

    Updated lsp to detect special lsp configs

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -50,9 +50,9 @@ for _, server in pairs(servers) do
     capabilities = require("plugins.lsp.handlers").capabilities
   }
   server = vim.split(server, "@")[1]
-  local require_ok, conf_opts = pcall(require, "plugins.lsp.settings" .. server)
+  local require_ok, conf_opts = pcall(require, "plugins.lsp.settings." .. server)
   if require_ok then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+    opts = conf_opts
   end
   nvim_lsp[server].setup(opts)
 end
--- lua/plugins/lsp/settings/ccls.lua
+++ lua/plugins/lsp/settings/ccls.lua
@@ -1,5 +1,7 @@
 local util = require 'lspconfig/util'
 return {
+  on_attach = require("plugins.lsp.handlers").on_attach,
+  capabilities = require("plugins.lsp.handlers").capabilities,
   root_dir = function(fname)
     return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname) or util.path.dirname(fname)
   end;
