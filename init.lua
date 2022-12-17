013264d60adc9edb8cd16df68bf1ecd1f7a7c140
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2022-12-17 06:38:02 +0530

    Added neovim info

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- NVIM v0.9.0-dev-312+gc4fcde506
+-- Build type: RelWithDebInfo
+-- LuaJIT 2.1.0-beta3
+
 ------------ Packer configs ---------------
 require('impatient').enable_profile()
 require('packer_nvim')
