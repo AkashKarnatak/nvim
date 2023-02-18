0b510d0c16491056666804cb91a271a863c4ca02
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2023-02-18 17:07:26 +0530

    Removed useless files

--- lua/plugins/sonokai.lua
+++
@@ -1,8 +0,0 @@
-vim.g.sonokai_style = 'default'
-vim.g.sonokai_enable_italic = 1
-vim.g.sonokai_disable_italic_comment = 0
-vim.g.sonokai_better_performance = 1
-vim.g.sonokai_menu_selection_background = "green"
-vim.g.sonokai_sign_column_background = 'none'
-vim.g.sonokai_show_eob = 1
-vim.cmd([[colorscheme sonokai]])
